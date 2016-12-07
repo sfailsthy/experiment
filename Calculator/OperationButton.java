0d8a753e737ef392ba6bdfeccb4850dcc805b55a
sfailsthy <z1823339284@gmail.com>
2016-12-07 23:00:09 +0800

    Add files via upload

---
+++ Calculator/OperationButton.java
@@ -0,0 +1,16 @@
+package sfailsthy;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class OperationButton extends JButton{
+   String operateSign;
+   public OperationButton(String s){
+      super(s);
+      operateSign=s;
+      setForeground(Color.red);
+   }
+   public String getOperateSign(){
+      return operateSign;
+   }
+}
